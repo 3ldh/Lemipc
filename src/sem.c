06491ec025e6189df2d218cfd1d64bf8e71e0b4b
Alexandre BLANCHARD <alexandre.blanchard@epitech.eu>
2017-03-30 15:17:08 +0200

    merge

--- src/sem.c
+++ src/sem.c
@@ -5,20 +5,20 @@
 ** Login   <mathieu.sauvau@epitech.eu>
 **
 ** Started on  Thu Mar 30 14:08:12 2017 Sauvau Mathieu
-** Last update Thu Mar 30 14:09:25 2017 Sauvau Mathieu
+** Last update Thu Mar 30 15:00:51 2017 Alexandre BLANCHARD
 */
 
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
-struct sembuf getsem_op(int sem_num, int op_value)
+struct sembuf	getsem_op(int sem_num, int op_value)
 {
   struct sembuf sops;
 
   sops.sem_num = sem_num;
   sops.sem_op = op_value;
   sops.sem_flg = 0;
-  return sops;
+  return (sops);
 }
 
 void		lock(int sem_id)
